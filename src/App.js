596245b7606e3d86fd676abce5981f0d9175fd07
Sunny Pig <upbeatliu@gmail.com>
2020-01-28 07:42:02 +1000

    fixed spinner and done to 22

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Alert from './components/layout/Alert'
 import Search from './components/layout/Search'
-import Spinner from './components/layout/Spinner'
 import Users from './components/users/Users'
 import User from './components/users/User'
 import About from './components/pages/About'
@@ -33,7 +32,7 @@ class App extends Component {
   }
 
   //get single Github user page
-  getUser = async (username) => {
+  getUser = async username => {
     this.setState({ loading: true });
 
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET_SECRET}`);
@@ -52,34 +51,30 @@ class App extends Component {
   render() {
     const { users, user, loading } = this.state;
 
-    if (this.state.loading) {
-      return <Spinner />
-    } else {
-      return (
-        <Router>
-          <div className="App">
-            <Navbar />
-            <div className="container">
-              <Alert alert={this.state.alert} />
-              <Switch>
-                <Route exact path='/' render={props => (
-                  <Fragment>
-                    <Search setAlert={this.setAlert} searchUsers={this.searchUsers}
-                      showClear={users.length > 0 ? true : false}
-                      clearUsers={this.clearUsers} />
-                    <Users loading={loading} users={users} />
-                  </Fragment>
-                )} />
-                <Route exact path='/about' render={About} />
-                <Route exact path='/user/:login' render={props => (
-                  <User {...props} getUser={this.getUser} user={user} loading={loading} />
-                )} />
-              </Switch>
-            </div>
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search setAlert={this.setAlert} searchUsers={this.searchUsers}
+                    showClear={users.length > 0 ? true : false}
+                    clearUsers={this.clearUsers} />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )} />
+              <Route exact path='/about' render={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} user={user} loading={loading} />
+              )} />
+            </Switch>
           </div>
-        </Router>
-      )
-    }
+        </div>
+      </Router>
+    )
   }
 }
 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
 
 const Users = ({ users, loading }) => {
-  return (
-    <div style={userStyle}>
-      {users.map(user =>
-        <UserItem key={user.id} user={user} />
-      )}
-    </div>
-  )
+  if (loading) {
+    return <Spinner />
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user =>
+          <UserItem key={user.id} user={user} />
+        )}
+      </div>
+    )
+  }
 }
 
 Users.prototype = {
